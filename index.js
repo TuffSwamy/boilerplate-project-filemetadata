17b240806c9c5f19b53314de58139e7a0ea97451
Adam Berrou <adazbebiz@gmail.com>
2024-10-29 23:28:54 +0000

    test

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
+var multer = require('multer');
 
 var app = express();
 
 app.use(cors());
+app.use(multer().single('upfile'));
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
-
+app.post('/api/fileanalyse', function (req, res) {
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  });
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
